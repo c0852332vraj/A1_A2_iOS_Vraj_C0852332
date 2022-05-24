c03beea2cd9aa5ed57f6102521ef067b43a1f096
Jinesh Patel <jineshp1142@gmail.com>
2022-05-24 18:02:21 -0400

    Initial Commit

---
+++ A1_A2_iOS_Vraj_C0852332/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A1_A2_iOS_Vraj_C0852332
+//
+//  Created by Vraj Patel on 24/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
